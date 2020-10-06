f4cb948543291fa9c7080f506a55d4280d6a3ab3
victorsc45 <victorlitzau@gmail.com>
2020-10-06 12:10:48 -0400

    deployed

---
+++ build/precache-manifest.504a8decf6512d3d2c90e2fad99b6521.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07e2f5b6354a78f1f8f0c728838bd09c",
+    "url": "/company-directory/index.html"
+  },
+  {
+    "revision": "7b77a22cb4bd9c92e68d",
+    "url": "/company-directory/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "22d12131a40f2ed3665e",
+    "url": "/company-directory/static/css/main.89064970.chunk.css"
+  },
+  {
+    "revision": "7b77a22cb4bd9c92e68d",
+    "url": "/company-directory/static/js/2.3c520343.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/company-directory/static/js/2.3c520343.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22d12131a40f2ed3665e",
+    "url": "/company-directory/static/js/main.3014a856.chunk.js"
+  },
+  {
+    "revision": "23dd6c47267a4d5581fc",
+    "url": "/company-directory/static/js/runtime-main.5fd81615.js"
+  }
+]);
