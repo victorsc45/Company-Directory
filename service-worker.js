cddf4aac700b0dc123e8225a5c2acaa794a55fee
victorsc45 <victorlitzau@gmail.com>
2020-10-08 18:17:11 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/company-directory/precache-manifest.783319cd1f084ebf711f71e6d21a3acc.js"
+  "/precache-manifest.a060c3d286609d543614e7ec09df6a49.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/company-directory/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
--- src/utils/API.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-export default {
-  getRandomUsers: function () {
-    return axios.get("https://randomuser.me/api/?results=200&nat=us");
-  }
-};
